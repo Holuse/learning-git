e5ec6cb1463c6d67ab57ca74fa5e901565e43f44
Holuse <mehmetemre09112006@gmail.com>
2025-08-27 19:32:35 +0300

    i am trying my best

--- learn_git.cpp
+++ learn_git.cpp
@@ -3,4 +3,7 @@
 int main(){
     std::cout << "its so hard to write on my laptops keyboad lmao" << std::endl;
     std::cout << "and lemme try a pull request" << std::endl;
+    for(int i = 0; i < 10; i++){
+        std::cout << "I have a feeling that git and github will make me miserable lmao" << std::endl;
+    }
 }
